75d0309fc0f1b85cc14ce85e626ff791eb27d28c
Jeferson Luis <jgjefersonluis@gmail.com>
2022-12-27 02:12:45 -0300

    javascript-validação

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+class Validator{
+
+    constructor(){
+        this.validations = [
+            'data-min-length',
+            'data-max-length',
+            'data-only-letters',
+            'data-email-validate',
+            'data-required',
+            'data-equal',
+            'data-password-validate'
+        ]
+    }
+
+// inicia a validação de todos os campos
+validate(form){
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
